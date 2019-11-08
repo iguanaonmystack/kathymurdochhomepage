f29c5601e70ea4dbf2ec45b54bb841dd535253c9
Kathy Murdoch <kathy@nivan.net>
2019-11-08 19:42:37 +0000

    Add lib/helpers.py for templates, omitted accidentally due to .gitignore

---
+++ kathymurdochhomepage/lib/helpers.py
@@ -0,0 +1,27 @@
+import re
+import cgi
+
+import bleach
+from flask import current_app as app
+
+def __getattr__(attr):
+    if attr == 'dev':
+        return app.debug
+    else:
+        raise AttributeError(attr)
+
+def tweet_markup(text):
+    """Escape markup, parse URIs into links."""
+    text = bleach.linkify(bleach.clean(text, strip=True))
+    return text
+
+def url(path):
+    '''replacement for old turbogears.url()'''
+    return path
+
+def config(value, default):
+    return app.config.get(value, default)
+
+class identity:
+    anonymous = True # TODO
+
